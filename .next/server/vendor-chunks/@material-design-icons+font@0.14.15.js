cfbf4019bb0fbc54982f56c899555d3a7b010dca
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-02 11:44:07 +0200

    Initiated Next js (via npx create-fire-cms

---
+++ .next/server/vendor-chunks/@material-design-icons+font@0.14.15.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@material-design-icons+font@0.14.15";
+exports.ids = ["vendor-chunks/@material-design-icons+font@0.14.15"];
+exports.modules = {
+
+/***/ "(ssr)/./node_modules/.pnpm/@material-design-icons+font@0.14.15/node_modules/@material-design-icons/font/filled.css":
+/*!********************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/@material-design-icons+font@0.14.15/node_modules/@material-design-icons/font/filled.css ***!
+  \********************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"7b288d3b9698\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvLnBucG0vQG1hdGVyaWFsLWRlc2lnbi1pY29ucytmb250QDAuMTQuMTUvbm9kZV9tb2R1bGVzL0BtYXRlcmlhbC1kZXNpZ24taWNvbnMvZm9udC9maWxsZWQuY3NzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxjQUFjO0FBQzdCLElBQUksS0FBVSxFQUFFLEVBQXVCIiwic291cmNlcyI6WyIvVXNlcnMva3Jpc3RvZmZlcndpbmRvbGYvU2l0ZXMvbXJrdy9ub2RlX21vZHVsZXMvLnBucG0vQG1hdGVyaWFsLWRlc2lnbi1pY29ucytmb250QDAuMTQuMTUvbm9kZV9tb2R1bGVzL0BtYXRlcmlhbC1kZXNpZ24taWNvbnMvZm9udC9maWxsZWQuY3NzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IFwiN2IyODhkM2I5Njk4XCJcbmlmIChtb2R1bGUuaG90KSB7IG1vZHVsZS5ob3QuYWNjZXB0KCkgfVxuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/.pnpm/@material-design-icons+font@0.14.15/node_modules/@material-design-icons/font/filled.css\n");
+
+/***/ })
+
+};
+;
