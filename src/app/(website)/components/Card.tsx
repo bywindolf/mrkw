60e379789e699c1652d562ee10994f9689deee5a
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-02 23:28:32 +0200

    Update card to just hold content

--- src/app/(website)/components/Card.tsx
+++ src/app/(website)/components/Card.tsx
@@ -10,9 +10,9 @@ type CardProps = {
 
 export default function Card({ item, className }: CardProps) {
   return (
-    <li className={`card ${className}`}>
+    <>
       {item.title}
       <p>{item.description}</p>
-    </li>
+    </>
   );
 }
