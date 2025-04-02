c918e89675f1ad725499ce234fbe8edc5d129e38
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-02 16:21:25 +0200

    Created some template data

--- src/app/(website)/components/Card.tsx
+++ src/app/(website)/components/Card.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Card() {
-  return <li>Card</li>;
+type CardProps = {
+  item: {
+    title: string;
+    description: string;
+  };
+};
+
+export default function Card({ item }: CardProps) {
+  return <li>{item.title}</li>;
 }
--- src/app/(website)/components/Work.tsx
+++ src/app/(website)/components/Work.tsx
@@ -1,16 +1,35 @@
+//Component: Card
+// Thinkig of making this Card "smart" for using with Experince maybe
+
 import React from "react";
 import Card from "./Card";
 
-const cardData = {};
+const cardData = [
+  {
+    id: 1,
+    title: "First Item",
+    description: "This is the first item description",
+  },
+  {
+    id: 2,
+    title: "Second Item",
+    description: "This is the second item description",
+  },
+  {
+    id: 3,
+    title: "Third Item",
+    description: "This is the third item description",
+  },
+];
 
 export default function Work() {
   return (
     <section className="work">
       <div className="work__container container">
         <ul className="work__list">
-          <Card />
-          <Card />
-          <Card />
+          {cardData.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
         </ul>
       </div>
     </section>
