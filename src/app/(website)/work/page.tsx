b00f9e53c5ceddb2a3c556b016b10805df213544
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 15:01:06 +0200

    Added proper elements and moved PageHeader out of Main

--- src/app/(website)/work/page.tsx
+++ src/app/(website)/work/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import PageHeader from "../components/PageHeader";
+import Main from "../components/Main";
+import Work from "../components/Work";
 
 export default function page() {
-  return <PageHeader>Asds</PageHeader>;
+  return (
+    <>
+      <PageHeader>Work</PageHeader>
+      <Main>
+        <Work></Work>
+      </Main>
+    </>
+  );
 }
