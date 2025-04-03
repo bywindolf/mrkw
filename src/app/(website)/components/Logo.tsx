57fd421581ef0a8517005db7671c858fb0258246
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 10:29:35 +0200

    Tailwind pre update

---
+++ src/app/(website)/components/Logo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <span className="logo__image">Logo</span>
+      <span className="logo__text">Text</span>
+    </div>
+  );
+}
