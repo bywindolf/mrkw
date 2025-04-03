3bd9d62667736c6f55122a0b2e8f654bc94815ca
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 14:54:07 +0200

    Update with styling for PageHeader Update component with proper classes (BEM)

--- src/app/(website)/components/PageHeader.tsx
+++ src/app/(website)/components/PageHeader.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function PageHeader({ children }: React.PropsWithChildren) {
   return (
     <section className="page-header">
-      <h1>{children}</h1>
+      <div className="page-header__container">
+        <h1>{children}</h1>
+      </div>
     </section>
   );
 }
