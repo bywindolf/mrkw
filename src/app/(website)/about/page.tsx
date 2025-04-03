6e081146bee2bfa8daa4014a7094fc286338e596
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 15:12:02 +0200

    Minor edit

---
+++ src/app/(website)/about/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageHeader from "../components/PageHeader";
+import Main from "../components/Main";
+
+export default function About() {
+  return (
+    <>
+      <PageHeader>About</PageHeader>
+      <Main>
+        <div className="container">Content to be added</div>
+      </Main>
+    </>
+  );
+}
