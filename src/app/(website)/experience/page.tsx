b6ace2e0ce4a192ae4c330812af0dfeaa68940e3
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 15:29:29 +0200

    Placeholder content added

--- src/app/(website)/experience/page.tsx
+++ src/app/(website)/experience/page.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import PageHeader from "../components/PageHeader";
 import Main from "../components/Main";
+import PageSubHeadline from "../components/PageSubHeadline";
+import Work from "../components/Work";
 
 export default function Experience() {
   return (
     <>
       <PageHeader>Experience</PageHeader>
       <Main>
-        <div className="container">Content to be added</div>
+        <PageSubHeadline>Professional</PageSubHeadline>
+        <Work />
+        <PageSubHeadline>Education</PageSubHeadline>
+        <Work />
       </Main>
     </>
   );
