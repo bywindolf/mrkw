4b58f13066f455dc2ed6f0b7a140daefebf215e6
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 11:55:30 +0200

    Making use of Tailwind predefined .container inside custom class. No need to define in component. Easily override behavioir setting within @theme (global)

--- src/app/(website)/components/Work.tsx
+++ src/app/(website)/components/Work.tsx
@@ -24,7 +24,7 @@ const cardData = [
 export default function Work() {
   return (
     <section className="work">
-      <div className="container work__container">
+      <div className="work__container">
         <ul className="grid work__list">
           {cardData.map((item) => (
             <li key={item.id} className="work__list-item">
