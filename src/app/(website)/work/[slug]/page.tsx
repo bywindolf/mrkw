b26214c4be7d358bf98f9a0048ee7cba7da3efd1
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 16:27:18 +0200

    Created Single Work page and route for dynamic routing using [slug]

---
+++ src/app/(website)/work/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageHeader from "@/app/(website)/components/PageHeader";
+import Main from "@/app/(website)/components/Main";
+
+export default function SingleWork() {
+  return (
+    <>
+      <PageHeader>Single Work</PageHeader>
+      <Main></Main>
+    </>
+  );
+}
