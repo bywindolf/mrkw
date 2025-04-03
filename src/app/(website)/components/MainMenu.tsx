45747b573a9c522a4ce6d79249eba44e174caf07
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 14:35:30 +0200

    Updated MenuItem with array that holds objects for each menu item for cleaner code.

--- src/app/(website)/components/MainMenu.tsx
+++ src/app/(website)/components/MainMenu.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import MenuItem from "./MenuItem";
 
+const menuData = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Work",
+    url: "/work",
+  },
+  {
+    id: 3,
+    title: "About",
+    url: "/about",
+  },
+];
+
 export default function MainMenu() {
   return (
     <nav className={"main-menu"}>
       <ul className="main-menu__list">
-        <MenuItem className="main-menu__list" url="/">
-          Home
-        </MenuItem>
-        <MenuItem className="main-menu__list" url="/test">
-          Test
-        </MenuItem>
+        {menuData.map((item) => (
+          <MenuItem url={item.url} className="main-menu__list-item">
+            {item.title}
+          </MenuItem>
+        ))}
       </ul>
     </nav>
   );
