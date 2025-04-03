444b34c6cec7360d70d1d8654cd2c4feab70210e
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 15:04:10 +0200

    Update Logo to be Link and link to /

--- src/app/(website)/components/Logo.tsx
+++ src/app/(website)/components/Logo.tsx
@@ -1,12 +1,13 @@
 //Component: Logo
 //BEM
+import Link from "next/link";
 import React from "react";
 
 export default function Logo() {
   return (
-    <div className="logo">
+    <Link className="logo" href={`/`}>
       <span className="logo__image">Logo</span>
       <span className="logo__text">Text</span>
-    </div>
+    </Link>
   );
 }
