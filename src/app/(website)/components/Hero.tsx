acf173b66bbd93c9f30a76fd27f5ec2f06078b30
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-03 11:59:25 +0200

    Created BEM class Updated Hero component with hero__container

--- src/app/(website)/components/Hero.tsx
+++ src/app/(website)/components/Hero.tsx
@@ -3,5 +3,9 @@
 import React, { PropsWithChildren } from "react";
 
 export default function Hero({ children }: PropsWithChildren) {
-  return <section className="hero">{children}</section>;
+  return (
+    <section className="hero">
+      <div className="hero__container">{children}</div>
+    </section>
+  );
 }
