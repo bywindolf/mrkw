f730d1c0915ffd67bc535f2b4ab909b78929beb8
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-04 14:51:44 +0200

    Created json data for skills, envs Created fetchDevProfile to fetch my json data Import fetchDevProfile inside page Experience

--- src/app/(website)/experience/page.tsx
+++ src/app/(website)/experience/page.tsx
@@ -3,11 +3,25 @@ import PageHeader from "../components/page-header";
 import Main from "../components/main";
 import PageSubHeadline from "../components/page-sub-headline";
 import Work from "../components/Work";
-export default function Experience() {
+import { fetchDevProfile } from "@/common/actions";
+
+export default async function Experience() {
+  const data = await fetchDevProfile();
+
+  console.log(data);
+
   return (
     <>
       <PageHeader>Experience</PageHeader>
+
       <Main>
+        {
+          //Iterate over my skills
+          data.skills.map((skill: string, index: number) => (
+            <div key="{index}">{skill}</div>
+          ))
+        }
+
         <PageSubHeadline>Professional</PageSubHeadline>
         <Work type="experience" />
         <PageSubHeadline>Education</PageSubHeadline>
