149799a2ec1e1fa50b85b50cb54a9b1cf7d049fc
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-04 13:36:54 +0200

    Created endpoint devProfile holding skills and environments

---
+++ src/common/api/dev-profile.ts
@@ -0,0 +1,17 @@
+export const devProfile = [
+  {
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "Tailwind CSS",
+      "Node.js",
+      "Git",
+      "Figma",
+    ],
+  },
+  {
+    environments: ["1", "2", "3", "4", "5", "6", "7", "8"],
+  },
+];
