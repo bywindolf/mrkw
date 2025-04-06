5925f62378b9b4263443d4772e2b64a109cdfdf8
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-06 19:55:06 +0200

    Some updates for my devProfile

--- src/app/(website)/experience/page.tsx
+++ src/app/(website)/experience/page.tsx
@@ -6,25 +6,45 @@ import Work from "../components/Work";
 import { fetchDevProfile } from "@/common/actions";
 
 export default async function Experience() {
+  //Get my devProfile
   const data = await fetchDevProfile();
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <>
       <PageHeader>Experience</PageHeader>
-
+      <section className="dev-profile">
+        <div className="dev-profile__container">
+          <div className="skills">
+            <h3>test</h3>
+            <ul className="skills__list">
+              {
+                //Skills may be used in other parts of app
+                //Iterate over my skills
+                data.skills.map((skill: string, index: number) => (
+                  <li key="{index}">{skill}</li>
+                ))
+              }
+            </ul>
+          </div>
+          <div className="environments">
+            <ul className="environments__list">
+              {
+                //Iterate over my skills
+                data.environments.map((env: string, index: number) => (
+                  <li key="{index}">{env}</li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+      </section>
       <Main>
-        {
-          //Iterate over my skills
-          data.skills.map((skill: string, index: number) => (
-            <div key="{index}">{skill}</div>
-          ))
-        }
-
         <PageSubHeadline>Professional</PageSubHeadline>
         <Work type="experience" />
         <PageSubHeadline>Education</PageSubHeadline>
+
         <Work type="experience" />
       </Main>
     </>
