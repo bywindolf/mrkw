d01deedd5140ae516fac240cc964ca3b846c562a
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-08 10:27:00 +0200

    Some CSS changes and tests

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,50 +1,54 @@
-import { db } from "@/lib/firebaseAdmin"; // import the initialized Firebase Admin SDK
-import React from "react";
-import PageHeader from "../components/page-header";
-import Main from "../components/main";
-import PageSubHeadline from "../components/page-sub-headline";
-import { ExperienceItem } from "@/common/types";
-import ExperienceCard from "../components/experience-card";
+import { db } from '@/lib/firebaseAdmin' // import the initialized Firebase Admin SDK
+import React from 'react'
+import PageHeader from '../components/page-header'
+import Main from '../components/main'
+import PageSubHeadline from '../components/page-sub-headline'
+import { ExperienceItem } from '@/common/types'
+import ExperienceCard from '../components/experience-card'
 // import { fetchDevProfile } from "@/common/actions";
 
 export default async function Experience() {
-  // Firebase way of fetching data for "collections" of data
-  const experienceSnapshot = await db.collection("experience").get();
-  const experienceData: ExperienceItem[] = experienceSnapshot.docs.map(
-    (doc) => ({
-      id: doc.id,
-      ...(doc.data() as Omit<ExperienceItem, "id">), // assuming id comes from doc.id
-    })
-  );
+    // Firebase way of fetching data for "collections" of data
+    const experienceSnapshot = await db.collection('experience').get()
+    const experienceData: ExperienceItem[] = experienceSnapshot.docs.map(
+        (doc) => ({
+            id: doc.id,
+            ...(doc.data() as Omit<ExperienceItem, 'id'>), // assuming id comes from doc.id
+        })
+    )
 
-  const eduationSnapshot = await db.collection("education").get();
-  const educationData: ExperienceItem[] = eduationSnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...(doc.data() as Omit<ExperienceItem, "id">), // assuming id comes from doc.id
-  }));
+    const eduationSnapshot = await db.collection('education').get()
+    const educationData: ExperienceItem[] = eduationSnapshot.docs.map(
+        (doc) => ({
+            id: doc.id,
+            ...(doc.data() as Omit<ExperienceItem, 'id'>), // assuming id comes from doc.id
+        })
+    )
 
-  return (
-    <>
-      <PageHeader>Experience</PageHeader>
+    return (
+        <>
+            <PageHeader>Experience</PageHeader>
 
-      <Main>
-        <PageSubHeadline>Professional</PageSubHeadline>
-        <ul className="experience__list">
-          {experienceData.map((item) => (
-            <li key={item.id} className="experience__item">
-              <ExperienceCard item={item} />
-            </li>
-          ))}
-        </ul>
-        <PageSubHeadline>Education</PageSubHeadline>
-        <ul className="experience__list">
-          {educationData.map((item) => (
-            <li key={item.id} className="experience__item">
-              <ExperienceCard item={item} />
-            </li>
-          ))}
-        </ul>
-      </Main>
-    </>
-  );
+            <Main>
+                <PageSubHeadline>Professional</PageSubHeadline>
+                <div className="container">
+                    <ul className="experience__list">
+                        {experienceData.map((item) => (
+                            <li key={item.id} className="experience__item">
+                                <ExperienceCard item={item} />
+                            </li>
+                        ))}
+                    </ul>
+                    <PageSubHeadline>Education</PageSubHeadline>
+                    <ul className="experience__list">
+                        {educationData.map((item) => (
+                            <li key={item.id} className="experience__item">
+                                <ExperienceCard item={item} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </Main>
+        </>
+    )
 }
