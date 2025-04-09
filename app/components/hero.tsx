87f8f3347bfeb3a066f2565eb78f7635dec58d46
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-09 10:15:23 +0200

    Added "." to trigger change

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,11 +1,11 @@
-//Component: Hero
+//Component: Hero.
 
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren } from 'react'
 
 export default function Hero({ children }: PropsWithChildren) {
-  return (
-    <section className="hero">
-      <div className="hero__container">{children}</div>
-    </section>
-  );
+    return (
+        <section className="hero">
+            <div className="hero__container">{children}</div>
+        </section>
+    )
 }
--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,4 +1,4 @@
-//Component: Logo
+//Component: Logo.
 //BEM
 import Image from 'next/image'
 import Link from 'next/link'
