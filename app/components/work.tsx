136aca1e8262c38974089eddc65a6ac3a323beb2
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-09 10:16:24 +0200

    Uncommented code in Work component

--- app/components/work.tsx
+++ app/components/work.tsx
@@ -9,22 +9,21 @@ import WorkCard from './work-card'
 import ExperienceCard from './experience-card'
 
 export default function Work({ type = 'work' }: WorkProps) {
-
     return (
-        // <section className={`${type}`}>
-        //     <div className={`${type}__container`}>
-        //         <ul className={`${type}__list`}>
-        //             {items.map((item) => (
-        //                 <li key={item.id} className={`${type}__list-item`}>
-        //                     {type === 'work' ? (
-        //                         <WorkCard item={item as WorkItem} />
-        //                     ) : (
-        //                         <ExperienceCard item={item as ExperienceItem} />
-        //                     )}
-        //                 </li>
-        //             ))}
-        //         </ul>
-        //     </div>
-        // </section>
+        <section className={`${type}`}>
+            <div className={`${type}__container`}>
+                <ul className={`${type}__list`}>
+                    {items.map((item) => (
+                        <li key={item.id} className={`${type}__list-item`}>
+                            {type === 'work' ? (
+                                <WorkCard item={item as WorkItem} />
+                            ) : (
+                                <ExperienceCard item={item as ExperienceItem} />
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
     )
 }
