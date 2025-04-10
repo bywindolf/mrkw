e8cc841d82871f7bf43478d1f30775ac08710632
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-10 15:12:08 +0200

    Active menu-item logic (useState) Added Tailwind @utility class for Grid issues and other Minor CSS

--- app/(routes)/work/[slug]/page.tsx
+++ app/(routes)/work/[slug]/page.tsx
@@ -19,27 +19,41 @@ export default async function SingleWork({ params }: SingleWorkProps) {
     const cover = page.cover?.[0]?.downloadURL || null
     return (
         <>
-            <section className="workinfo">
-                <div className="workinfo__container">
-                    <div className="workinfo__info">
-                        <div className="workinfo__header">
-                            <h1 className="header__client">{page.client}</h1>
-                            <h2 className="header__title">S{page.title}</h2>
+            <Main>
+                <section className="workinfo">
+                    <div className="workinfo__container">
+                        <div className="workinfo__info">
+                            <div className="workinfo__header">
+                                <h1 className="header__client">
+                                    {page.client}
+                                </h1>
+                                <h2 className="header__title">{page.title}</h2>
+                            </div>
+                            <div className="workinfo__details">
+                                <ul className="workinfo__categories">
+                                    <li>Category A</li>
+                                    <li>Category B</li>
+                                </ul>
+                                <p className="workinfo__timespan">20XX</p>
+                            </div>
                         </div>
-                        <div className="workinfo__details">
-                            <ul className="workinfo__categories">
-                                <li>Category A</li>
-                                <li>Category B</li>
-                            </ul>
-                            <p className="workinfo__timespan">20XX</p>
+                        <div className="workinfo__description">
+                            {page.description}
                         </div>
                     </div>
+                </section>
+                <div className="my-container">
+                    {' '}
+                    {cover && (
+                        <Image
+                            width={1000}
+                            height={122}
+                            src={cover}
+                            alt=""
+                        ></Image>
+                    )}
                 </div>
-                {cover && (
-                    <Image width={1000} height={122} src={cover} alt=""></Image>
-                )}
-            </section>
-            <Main>test</Main>
+            </Main>
         </>
     )
 }
--- app/components/global-header.tsx
+++ app/components/global-header.tsx
@@ -1,15 +1,23 @@
+'use client'
 //Component GlobalHeader
 
-import React from 'react'
+import React, { useState } from 'react'
 import MainMenu from './main-menu'
 import Logo from './logo'
+import Togglemenu from './toggle-menu'
 
 export function GlobalHeader() {
+    //To hold states for our Toggle
+    const [menuVisible, setMenuVisible] = useState(false)
     return (
-        <header className={'global-header'}>
+        <header className={'global-header'} data-toggle={menuVisible}>
             <div className="global-header__container">
                 <Logo />
-                <MainMenu />
+                <MainMenu menuVisible={menuVisible} />
+                <Togglemenu
+                    menuVisible={menuVisible}
+                    setMenuVisible={setMenuVisible}
+                ></Togglemenu>
             </div>
         </header>
     )
