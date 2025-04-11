87a6d322c7bb5017482b57fd49723719d5e31043
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-11 15:46:20 +0200

    Added test for trigger workflow

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <Main>
                 <Hero>
                     <h1>
-                        ?Designer embrazing a updated role as frontend
+                        ..Designer embrazing a updated role as frontend
                         developer.
                     </h1>
                     <p>Ok</p>
