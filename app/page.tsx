c09bef2a4e9ff9bb5f7722834b5d604941abd9b5
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-15 22:22:36 +0200

    Added force-auto

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+//https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+export const dynamic = 'force-auto'
 import React from 'react'
 import Link from 'next/link'
 import Main from '@components/layout/main'
