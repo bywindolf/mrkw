7de33b9207cd5b37296fa01590fb326ab2c94f20
Kristoffer Windolf <kristoffer.windolf@gmail.com>
2025-04-20 00:01:31 +0200

    Updated package.json

--- my-cms/src/components/StorageImage.tsx
+++ my-cms/src/components/StorageImage.tsx
@@ -1,40 +1,41 @@
-import React, { useEffect, useState } from "react";
-import { useStorageSource } from "@firecms/core";
-import { cls } from "@firecms/ui";
+import React, { useEffect, useState } from 'react'
+import { useStorageSource } from '@firecms/core'
+import { cls } from '@firecms/ui'
 
 /**
  * This is a simple component that renders an image from a Firebase Storage path.
  */
 export function StorageImage({
-                                 storagePath,
-                                 style,
-                                 className,
-                                 alt
-                             }: {
-    alt?: string,
-    storagePath: string,
-    style?: React.CSSProperties,
+    storagePath,
+    style,
+    className,
+    alt,
+}: {
+    alt?: string
+    storagePath: string
+    style?: React.CSSProperties
     className?: string
 }) {
-
-    const storage = useStorageSource();
-    const [url, setUrl] = useState<string | undefined>();
+    const storage = useStorageSource()
+    const [url, setUrl] = useState<string | undefined>()
     useEffect(() => {
         if (storagePath) {
-            storage.getDownloadURL(storagePath)
-                .then((res) => setUrl(res.url ?? undefined));
+            storage
+                .getDownloadURL(storagePath)
+                .then((res) => setUrl(res.url ?? undefined))
         }
-    }, [storage, storagePath]);
+    }, [storage, storagePath])
 
-    if (!storagePath)
-        return <></>;
+    if (!storagePath) return <></>
 
     return (
         <img
-            alt={alt ?? "Generic"}
-            className={cls("object-contain", className)}
+            alt={alt ?? 'Generic'}
+            className={cls('object-contain', className)}
             style={{
-                ...style
-            }} src={url}/>
-    );
+                ...style,
+            }}
+            src={url}
+        />
+    )
 }
